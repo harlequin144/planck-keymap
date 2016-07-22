3ec2d01144bc1d32974033c3cd3851e9727dd98d
Thompson <DyThompson@hco.hc.hctx.net>
2016-07-22 10:14:26 -0500

    Forking the repo to start tracking my config changes.

---
+++ keyboards/planck/keymaps/dylr/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// place overrides here
+#define PREVENT_STUCK_MODIFIERS
+#define DISABLE_SPACE_CADET_ROLLOVER
+
+#endif
